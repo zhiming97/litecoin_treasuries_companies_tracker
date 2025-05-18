8bb7a188466a1d0d4fd753411217d1b4b6891254
Diamondhands <97498951+zhiming97@users.noreply.github.com>
2025-05-18 12:11:34 +0800

    Update page.tsx

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,30 +1,8 @@
-'use client'
+import { createClient } from '@/utils/supabase/server'
 
-import { createClient } from '@/utils/supabase/client'
-import { useEffect, useState } from 'react'
-
-export default function Page() {
-  const [notes, setNotes] = useState<any[] | null>(null)
-  const supabase = createClient()
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const { data, error } = await supabase.from('notes').select()
-        if (error) {
-          console.error('Error fetching notes:', error)
-          return
-        }
-        setNotes(data)
-      } catch (error) {
-        console.error('Error:', error)
-      }
-    }
-    getData()
-  }, [])
-
-  if (!notes) return <div>Loading...</div>
-  if (notes.length === 0) return <div>No notes found. Make sure you have added some notes to your database.</div>
+export default async function Page() {
+  const supabase = await createClient()
+  const { data: notes } = await supabase.from('notes').select()
 
   return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
