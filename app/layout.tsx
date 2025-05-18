6ce0f19e3c4ca2d5e841faed3bac7ba4923421c9
Diamondhands <97498951+zhiming97@users.noreply.github.com>
2025-05-18 11:53:54 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,7 @@ export default function RootLayout({
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Next.js Supabase Starter</Link>
+                    <Link href={"/notes"}>Notes</Link>
                     <div className="flex items-center gap-2">
                       <DeployButton />
                     </div>
