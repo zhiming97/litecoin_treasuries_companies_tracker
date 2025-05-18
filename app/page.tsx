ba6e06882ae05e04c7650d1f1093565af81fad41
Diamondhands <97498951+zhiming97@users.noreply.github.com>
2025-05-18 11:30:49 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { createClient } from '@/utils/supabase/server'
+
 
 export default async function Home() {
   return (
@@ -13,4 +15,13 @@ export default async function Home() {
       </main>
     </>
   );
+}
+
+
+
+export default async function Page() {
+  const supabase = await createClient()
+  const { data: notes } = await supabase.from('notes').select()
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
